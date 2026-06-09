8b45fb66a34a71be0de7bb9d6e13a4f259dacfe7
kundanpure <kundankumar8797737@gmail.com>
2026-06-09 21:41:30 +0530

    added google oauth

--- src/main/java/com/travyn/chat/config/WebSocketAuthInterceptor.java
+++ src/main/java/com/travyn/chat/config/WebSocketAuthInterceptor.java
@@ -16,7 +16,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 /**
- * Intercepts STOMP CONNECT frames to validate JWT tokens.
+ * Interceptss STOMP CONNECT frames to validate JWT tokens.
  * Clients must send the JWT token in the "Authorization" header of the STOMP CONNECT frame.
  */
 @Component
